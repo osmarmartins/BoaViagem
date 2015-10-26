46ea1cc108a0245648096309d91dc2b3547b979c
Osmar Martins Filho <osmarfilho6@gmail.com>
2015-10-26 06:56:36 -0300

    Criação do repositório

---
+++ app/src/main/java/br/edu/fa7/boaviagem/BoaViagemActivity.java
@@ -0,0 +1,44 @@
+package br.edu.fa7.boaviagem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class BoaViagemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_boa_viagem, menu);
+        return true;
+    }
+
+    public void btAcessarOnClick(View view){
+        Toast.makeText(this, "Botão de Login Acionado! ", Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
